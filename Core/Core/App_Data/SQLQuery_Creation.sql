545d1a1e1a061b9f5dcfd351a3f654445294d66c
Angel Guzman <angelguzmangomez@gmail.com>
2020-09-24 21:48:59 -0400

    Creacion tablas Core

--- Core/Core/App_Data/SQLQuery_Creation.sql
+++ Core/Core/App_Data/SQLQuery_Creation.sql
@@ -1,4 +1,6 @@
-﻿CREATE TABLE [dbo].[Tbl_Usuario]
+﻿--drop table Tbl_Usuario
+
+CREATE TABLE [dbo].[Tbl_Usuario]
 (
 	[Id] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	[UserName] VARCHAR(25) NOT NULL,
@@ -26,9 +28,110 @@ CREATE TABLE [dbo].[Tbl_Beneficiario]
 
 GO
 
+CREATE TABLE [dbo].[Tbl_Cuenta]
+(
+	[Id] INT NOT NULL IDENTITY(1, 1),
+	[ClienteId] INT NOT NULL,
+	[Monto] Decimal(20,2) NOT NULL,
+	[FechaCreacion] Datetime NOT NULL,
+	PRIMARY KEY(Id)
+)
 
-ALTER TABLE [Tbl_Beneficiario]
-	ADD FOREIGN KEY (IdCuenta) REFERENCES Tbl_Cuenta(Id);
+GO
 
-ALTER TABLE [Tbl_Beneficiario]
-	ADD FOREIGN KEY (IdCuentaDestino) REFERENCES Tbl_Cuenta(Id);
+CREATE TABLE [dbo].[Tbl_Cliente]
+(
+	[Id] INT NOT NULL IDENTITY(1, 1),
+	[Direccion] varchar NOT NULL,
+	[FechaCreacion] DATETIME NOT NULL,
+	[UsuarioId] Int NOT NULL,
+	PRIMARY KEY(Id)
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_BeneficiarioEstado]
+(
+	[Id] SmallINT IDENTITY(1, 1) NOT NULL,
+	[Nombre] varchar NOT NULL,
+	[FechaCreacion] DATETIME NOT NULL
+	PRIMARY KEY(Id)
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_Transaccion]
+(
+	[Id] BigINT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[TipoTransaccionId] smallint NOT NULL,
+	[Monto] decimal(20,2) NOT NULL,
+	[Fecha] DATETIME NOT NULL,
+	[CuentaId] INT NOT NULL,
+	[CajaId] INT NOT NULL
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_TipoTransaccion]
+(
+	[Id] SmallINT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[Nombre] varchar NOT NULL
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_EstadoUsuario]
+(
+	[Id] SmallINT IDENTITY(1, 1) NOT NULL,
+	[Nombre] varchar NOT NULL,
+	[Estado] bit NOT NULL
+	PRIMARY KEY(Id)
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_Perfil]
+(
+	[Id] INT IDENTITY(1, 1) NOT NULL,
+	[Nombre] varchar NOT NULL,
+	[Descripcion] varchar NOT NULL,
+	[Estado] bit NOT NULL
+	PRIMARY KEY(Id)
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_Caja]
+(
+	[Id] INT IDENTITY(1, 1) NOT NULL,
+	[Descripcion] varchar NOT NULL,
+	[FechaCreacion] Datetime NOT NULL,
+	[Monto] decimal(20,2) NOT NULL,
+	[Estado] smallint NOT NULL
+	PRIMARY KEY(Id)
+)
+
+CREATE TABLE [dbo].[Tbl_OperacionCaja]
+(
+	[Id] smallINT IDENTITY(1, 1) NOT NULL,
+	[TipoId] smallint NOT NULL,
+	[Fecha] Datetime NOT NULL,
+	[Monto] decimal(20,2) NOT NULL,
+	[Estado] bit NOT NULL
+	PRIMARY KEY(Id)
+)
+
+GO
+
+CREATE TABLE [dbo].[Tbl_OperacionCajaTipo]
+(
+	[Id] smallINT IDENTITY(1, 1) NOT NULL,
+	[Nombre] varchar NOT NULL,
+	[Estado] bit NOT NULL
+	PRIMARY KEY(Id)
+)
+--ALTER TABLE [Tbl_Beneficiario]
+	--ADD FOREIGN KEY (IdCuenta) REFERENCES Tbl_Cuenta(Id);
+
+--ALTER TABLE [Tbl_Beneficiario]
+	--ADD FOREIGN KEY (IdCuentaDestino) REFERENCES Tbl_Cuenta(Id);
