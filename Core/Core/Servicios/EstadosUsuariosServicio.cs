1e535e6cc602d8832602b2aa7453addbbd074e15
Fernando Grullon <fernandogrullon04@gmail.com>
2020-10-03 19:07:16 -0400

    se creo estados de usuarios y beneficiarios

---
+++ Core/Core/Servicios/EstadosUsuariosServicio.cs
@@ -0,0 +1,60 @@
+﻿using Core.ModeloData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Servicios
+{
+     public class EstadosUsuariosServicio
+    {
+        public BanCovid_DBEntities _db;
+        public EstadosUsuariosServicio()
+        {
+            _db = new BanCovid_DBEntities();
+        }
+
+
+        public void Crear(Tbl_EstadoUsuario modelo) 
+        {
+
+            modelo.Estado = true;
+            _db.Tbl_EstadoUsuario.Add(modelo);
+
+            _db.SaveChanges();
+
+        }
+
+       
+        public List<Tbl_EstadoUsuario> ObtenerTodos()
+        {
+            return _db.Tbl_EstadoUsuario.ToList();
+        }
+
+        
+        public Tbl_EstadoUsuario Obtener(int id)
+        {
+            return _db.Tbl_EstadoUsuario.SingleOrDefault(x => x.Id == id);
+        }
+
+        public void Editar(Tbl_EstadoUsuario modelo)
+        {
+            var registro = _db.Tbl_EstadoUsuario.SingleOrDefault(x => x.Id == modelo.Id);
+
+            if (string.IsNullOrWhiteSpace(modelo.Nombre))
+                registro.Nombre = modelo.Nombre;
+
+            _db.SaveChanges();
+        }
+
+        public void Eliminar(int id)
+        {
+            var registro = _db.Tbl_EstadoUsuario.SingleOrDefault(x => x.Id == id);
+
+            registro.Estado = false;
+
+            _db.SaveChanges();
+        }
+    }
+}
