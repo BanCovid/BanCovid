49064d1226ab3d4ad47a13508e157e19e7869368
Jordy Mateo <jordymateo@hotmail.es>
2020-10-06 16:20:17 -0400

    Modificadas las tablas y creadas relaciones

---
+++ Core/Core/General/Seguridad.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.General
+{
+    public class Seguridad
+    {
+        public static string EncriptarContrasena(string password, short iterations = 1000)
+        {
+            //generate a random salt for hashing
+            var salt = new byte[24];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+
+            //hash password given salt and iterations (default to 1000)
+            //iterations provide difficulty when cracking
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            byte[] hash = pbkdf2.GetBytes(24);
+
+            //return delimited string with salt | #iterations | hash
+            return Convert.ToBase64String(salt) + "|" + iterations + "|" +
+                Convert.ToBase64String(hash);
+
+        }
+        public static bool Validar(string testPassword, string encriptedPass)
+        {
+            //extract original values from delimited hash text
+            var origHashedParts = encriptedPass.Split('|');
+            var origSalt = Convert.FromBase64String(origHashedParts[0]);
+            var origIterations = Int32.Parse(origHashedParts[1]);
+            var origHash = origHashedParts[2];
+
+            //generate hash from test password and original salt and iterations
+            var pbkdf2 = new Rfc2898DeriveBytes(testPassword, origSalt, origIterations);
+            byte[] testHash = pbkdf2.GetBytes(24);
+
+            //if hash values match then return success
+            if (Convert.ToBase64String(testHash) == origHash)
+                return true;
+
+            //no match return false
+            return false;
+
+        }
+    }
+}
